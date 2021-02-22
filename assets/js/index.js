034552e8a8d1962c7efbee8a90a9acf7e1e62de5
BodNS <bondarenokn@gmail.com>
2021-02-22 17:52:05 +0200

    w22.02

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,24 +2,69 @@ const newTask = document.querySelector("input[name=task]");
 const tskList = document.getElementById("tasksList");
 const addBtn = document.getElementById("addTaskButton");
 const delBtn = document.getElementById("deleteTaskButton");
+let liTaskElem = document.querySelectorAll("taskElem");
 const myRegExp = /\W/;
+let isValid = false;
 
 newTask.oninput = onInputHandler;
 addBtn.addEventListener("click", addNewTask);
+delBtn.addEventListener("click", delTask);
+
+
+
+function onInputHandler(event) {
+  if (!event.target.value) {
+    event.target.classList.remove("validTask");
+    event.target.classList.remove("invalidTask");
+    event.target.classList.add("emptyInput");
+} else {
+ event.target.classList.remove("invalidTask");
+  event.target.classList.remove("validTask");
+  isValid = !myRegExp.test(event.target.value);
+  event.target.classList.add(isValid ? "validTask" : "invalidTask");
+}
+}
 
 function addNewTask(event) {
-  if (newTask.value) {
+  if (newTask.value && isValid) {
     const liNewTask = document.createElement("li");
     liNewTask.classList.add("taskElem");
     liNewTask.textContent = newTask.value;
     tskList.append(liNewTask);
     newTask.value = "";
+    newTask.classList.remove("validTask");
+    newTask.classList.add("emptyInput");
   }
   return;
 }
 
-function onInputHandler(event) {
-  let isValid = !myRegExp.test(event.target.value);
-  console.log(this.value);
-  event.target.className = isValid ? "validTask" : "invalidTask";
+function highlightElement (event) {
+event.target.classList.remove("taskElem");
+    event.target.classList.add("highlightElem");
 }
+
+function delTask (event) {
+
+
+}
+
+function liMonitoring () {
+  setInterval(() => {
+    liTaskElem = document.querySelectorAll("taskElem");
+for (let li of liTaskElem) {
+  li.addEventListener("click", highlightElement);
+}
+  }, 1000);
+}
+
+liMonitoring (); 
+/* const divBlock = document.querySelectorAll(".changeColor");
+
+for (let elem of divBlock) {
+  elem.insertAdjacentHTML(
+    "afterbegin",
+    '<button class ="remove_elem">X</button>'
+  );
+  elem.firstChild.onclick = () => elem.remove();
+  elem.addEventListener("click", changeColor1);
+} */
